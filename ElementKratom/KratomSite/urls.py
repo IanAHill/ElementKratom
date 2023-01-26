ad08af287f3b79db214d95b23888b7c1772da9fe
Ian <ian@mag.industries>
2023-01-26 08:56:23 -0500

    pushing to github

--- ElementKratom/KratomSite/urls.py
+++ ElementKratom/KratomSite/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('products/', views.products, name='products'),
+    path('productpage/', views.product_page, name="product_page"),
 
 ]
--- ElementKratom/KratomSite/views.py
+++ ElementKratom/KratomSite/views.py
@@ -2,8 +2,15 @@ from django.shortcuts import render
 
 # Create your views here.
 
+red_bali = {
+    "description": "of Element Kratom as this Red Bali ignites your senses like a wildfire"
+}
+
 def home(request):
     return render(request, "home.html", {})
 
 def products(request):
     return render(request, "products.html", {})
+
+def product_page(request):
+    return render(request, "product-page.html", {})
